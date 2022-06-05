b0e620adca353a0a8a0e85431909e71fb0ab5797
strong__cat <Alonso98>
2022-06-05 09:49:38 +0200

    added Singleton

---
+++ src/Patterns/Singleton/Singleton.java
@@ -0,0 +1,23 @@
+package Patterns.Singleton;
+
+public final class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+
+
+    /**
+     *  Double check for Threads.
+     */
+    public static Singleton getInstance(){
+        if (instance!=null){
+            synchronized (Singleton.class) {
+                if (instance!=null)
+                    instance = new Singleton();
+            }
+
+        }
+        return instance;
+    }
+}
